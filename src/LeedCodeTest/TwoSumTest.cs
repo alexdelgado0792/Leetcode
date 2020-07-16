408382ac0bbd9fb6a8a9616f99d875ea6fa5e384
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-16 13:05:29 -0500

    Test Project
    init test project for all problems

---
+++ src/LeedCodeTest/TwoSumTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeedCodeTest
+{
+    public class TwoSumTest
+    {
+        [Fact]
+        public void TwoSum() 
+        { 
+        }
+    }
+}
