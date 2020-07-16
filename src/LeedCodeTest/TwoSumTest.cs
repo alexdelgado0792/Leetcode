89f80ef66f8aceefaebf55868da1c892cd4401ed
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-16 15:46:11 -0500

    Update TwoSumTest.cs

--- src/LeedCodeTest/TwoSumTest.cs
+++ src/LeedCodeTest/TwoSumTest.cs
@@ -7,6 +7,7 @@ namespace LeedCodeTest
     {
         [Theory]
         [InlineData(new object[] { new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 } })]
+        [InlineData(new object[] { new int[] { 2, 5, 11, 15 }, 9, new int[] { 0, 0 } })]
         public void TwoSum(int[] nums, int target, int[] result)
         {
             Assert.Equal(result, Solution.TwoSum(nums, target));
