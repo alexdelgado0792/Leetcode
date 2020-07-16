07dbce88133eebf184eeee81544fb59fbfec3d07
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-16 15:51:31 -0500

    Update TwoSumTest.cs
    split test

--- src/LeedCodeTest/TwoSumTest.cs
+++ src/LeedCodeTest/TwoSumTest.cs
@@ -1,4 +1,5 @@
 ﻿using Problems.TwoSum;
+using System;
 using Xunit;
 
 namespace LeedCodeTest
@@ -7,10 +8,16 @@ namespace LeedCodeTest
     {
         [Theory]
         [InlineData(new object[] { new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 } })]
-        [InlineData(new object[] { new int[] { 2, 5, 11, 15 }, 9, new int[] { 0, 0 } })]
-        public void TwoSum(int[] nums, int target, int[] result)
+        public void TwoSumPass(int[] nums, int target, int[] result)
         {
             Assert.Equal(result, Solution.TwoSum(nums, target));
+        }
+
+        [Theory]
+        [InlineData(new object[] { new int[] { 2, 5, 11, 15 }, 9})]
+        public void TwoSumNotPass(int[] nums, int target)
+        {
+            Assert.Throws<Exception>(() => Solution.TwoSum(nums, target));
         }
     }
 }
