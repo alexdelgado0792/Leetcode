445952ce9c9c9be9ca16e3ebad9af0ce87da95e3
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-16 20:11:06 -0500

    Test Add two numbers
    update test  assert

---
+++ src/LeedCodeTest/AddTwoNumbers.cs
@@ -0,0 +1,25 @@
+﻿using Problems.AddTwoNumbers;
+using Xunit;
+
+namespace LeedCodeTest
+{
+    public class AddTwoNumbers
+    {
+        [Fact]
+        public void AddTwoNumbersPass()
+        {
+            var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+            var r = new ListNode(7, new ListNode(0, new ListNode(8)));
+            var solve = Solution.AddTwoNumbers(l1, l2);
+
+            while (r != null)
+            {
+                Assert.Equal(r.val, solve.val); ;
+                r = r.next;
+                solve = solve.next;
+            }
+        }
+
+    }
+}
