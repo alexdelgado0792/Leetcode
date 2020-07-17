3362ba3380ea661599f6ea0b3f8f66e3936c4811
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-17 15:48:23 -0500

    Create ReverseInteger.cs
    update test data

---
+++ src/LeedCodeTest/ReverseInteger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Problems.ReverseInteger;
+namespace LeedCodeTest
+{
+    public class ReverseInteger
+    {
+        [Theory]
+        [InlineData(123,321)] 
+        [InlineData(-123,-321)] 
+        [InlineData(120,21)]
+        [InlineData(int.MaxValue, 7463847412)]
+        [InlineData(int.MinValue, -8463847412)]
+        public void Pass(int number, int result)
+        {
+            Assert.Equal(result, Solution.Reverse(number));
+        }
+    }
+}
