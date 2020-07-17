12a034d57044b09f727430bdd02c2b735e9cff09
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-17 10:15:11 -0500

    Longest Substring Without Repeating Characters
    init class and test

---
+++ src/LeedCodeTest/LongestSubStringWithOutRepetingCharacters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Problems.LongestSubStringWithOutRepetingCharacters;
+
+namespace LeedCodeTest
+{
+    public class LongestSubStringWithOutRepetingCharacters
+    {
+        [Theory]
+        [InlineData("abcabcbb",3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        public void LongestSubStringPass(string input, int result)
+        {
+            Assert.Equal(result, Solution.LengthOfLongestSubstring(input));
+        }
+    }
+}
---
+++ src/Problems/LongestSubStringWithOutRepetingCharacters/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.LongestSubStringWithOutRepetingCharacters
+{
+    public static class Solution
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            return 0;
+        }
+    }
+}
