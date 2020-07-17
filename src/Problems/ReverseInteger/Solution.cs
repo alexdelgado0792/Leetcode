6810c3c49ab97bb34ea7db00e1495406f8ab0dfc
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-17 15:48:41 -0500

    Create Solution.cs
    ReverseInteger first approach

---
+++ src/Problems/ReverseInteger/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.ReverseInteger
+{
+    public static class Solution
+    {
+        public static int Reverse(int x)
+        {
+            string numberText = Math.Abs(x).ToString();
+            string result = string.Empty;
+
+            for (int i = numberText.Length - 1; i >= 0; i--)
+            {
+                result += numberText[i];
+            }
+
+            try 
+            {
+                int number = int.Parse(result);
+                return (x > 0) ? number : (number * -1);
+            }
+            catch (OverflowException)
+            {
+                return 0;
+            }
+        }
+    }
+}
