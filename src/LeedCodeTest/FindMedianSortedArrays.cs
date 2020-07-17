e6bba0d146659ebfdcf4de34958c9c3ce83d987e
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-17 14:56:16 -0500

    Problem 4 Leetcode
    first approch , missing negative behavior

---
+++ src/LeedCodeTest/FindMedianSortedArrays.cs
@@ -0,0 +1,18 @@
+﻿using Problems.MediumOfTwoSortedArray;
+using Xunit;
+
+namespace LeedCodeTest
+{
+    public class FindMedianSortedArrays
+    {
+        [Theory]
+        [InlineData(new object[] { new int[] { 1, 3 }, new int[] { 2 }, 2.0 })]
+        [InlineData(new object[] { new int[] { 1, 2 }, new int[] { 3, 4 }, 2.5 })]
+        [InlineData(new object[] { new int[] { 40000, 29000, 35500, 31000, 43000 }, new int[] { 30000, 27000, 32000 }, 31500 })]
+        [InlineData(new object[] { new int[] { 3 }, new int[] { -2,-1}, -1.0 })] // caso faltante
+        public void Pass(int[] nums1, int[] nums2, double result)
+        {
+            Assert.Equal(result, Solution.FindMedianSortedArrays(nums1, nums2));
+        }
+    }
+}
---
+++ src/Problems/MediumOfTwoSortedArray/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Problems.MediumOfTwoSortedArray
+{
+    public static class Solution
+    {
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] numbers = nums1.Concat(nums2).ToArray();
+            Array.Sort(numbers);
+            double result;
+
+            if ((nums1.Length + nums2.Length) % 2 == 0)
+            {
+                var rIndex = numbers.Length / 2;
+                var lIndex = rIndex - 1;
+
+                result = (numbers[rIndex] + numbers[lIndex]) / 2.0;
+            }
+            else
+            {
+                result = (double) (nums1.Sum() + nums2.Sum()) / numbers.Length;
+            }
+
+            return result;
+        }
+    }
+}
