61a04c2332e3109e6540a6afbb2ae22243ffcc9b
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-17 20:00:10 -0500

    Roman to integer problem
    upload test and code template

---
+++ src/LeedCodeTest/RomanToInteger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Problems.RomanToInteger;
+
+namespace LeedCodeTest
+{
+    public class RomanToInteger
+    {
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        public void Pass(string RomanNumber, long result)
+        {
+            Assert.Equal(result, Solution.RomanToInt(RomanNumber));
+        }
+    }
+}
---
+++ src/Problems/RomanToInteger/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.RomanToInteger
+{
+    public static class Solution
+    {
+        public static int RomanToInt(string s)
+        {
+            return 0;
+        }
+    }
+}
