cbe1a1ecec98303ece2876d48a4bcc632677e997
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-20 12:00:07 -0500

    Longest Common Prefix
    test and solution

---
+++ src/LeedCodeTest/LongestCommonPrefix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Problems.LongestCommonPrefix;
+
+namespace LeedCodeTest
+{
+    public class LongestCommonPrefix
+    {
+        [Theory]
+        [InlineData(new object[] { new string[] { "flower", "flow", "flight" }, "fl" })]
+        [InlineData(new object[] { new string[] { "dog", "racecar", "car" }, "" })]
+        public void Pass(string[] phrases, string result)
+        {
+            Assert.Equal(result, Solution.LongestCommonPrefix(phrases));
+        }
+    }
+}
---
+++ src/Problems/LongestCommonPrefix/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems.LongestCommonPrefix
+{
+    public static class Solution
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            string prefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+
+                    if (prefix == string.Empty)
+                    {
+                        return string.Empty;
+                    }
+                }
+            }
+
+            return prefix;
+        }
+    }
+}
