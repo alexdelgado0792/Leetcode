59e5276cba008f282ea0ef042071010626c716b5
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-20 15:21:24 -0500

    Valid Parentheses
    problem solution and test

---
+++ src/LeedCodeTest/ValidParentheses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Problems.Valid_Parentheses;
+
+namespace LeedCodeTest
+{
+    public class ValidParentheses
+    {
+        [Theory]
+        [InlineData("{()}", true)]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        [InlineData("([)]", false)]
+        [InlineData("{([{{()}}])}", true)]
+        [InlineData("", true)]
+        [InlineData("){", false)]
+        public void TwoSumPass(string input, bool result)
+        {
+            Assert.Equal(result, Solution.IsValid(input));
+        }
+    }
+}
---
+++ src/Problems/Valid Parentheses/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Valid_Parentheses
+{
+    public static class Solution
+    {
+        public static bool IsValid(string s)
+        {
+            if (s.Length == 0) return true;
+            if (s.Length == 1) return false;
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char character in s)
+            {
+                if ((character == '}' || character == ']' || character == ')') && stack.Count > 0)
+                {
+                    
+                    if (character == '}' && stack.Pop() != '{')
+                    {
+                        return false;
+                    }
+                    if (character == ']' && stack.Pop() != '[')
+                    {
+                        return false;
+                    }
+                    if (character == ')' && stack.Pop() != '(')
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(character);
+                }
+
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
