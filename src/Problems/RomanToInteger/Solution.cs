2331fad5b3c64689e34670fb872f5454e0c9fce6
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-20 11:00:40 -0500

    Update Solution.cs
    solution for problem Roman to integer

--- src/Problems/RomanToInteger/Solution.cs
+++ src/Problems/RomanToInteger/Solution.cs
@@ -8,7 +8,26 @@ namespace Problems.RomanToInteger
     {
         public static int RomanToInt(string s)
         {
-            return 0;
+            var romanDic = new Dictionary<char, int>()
+            {
+                { 'I' , 1 },{ 'V' , 5 },{ 'X' , 10 },{ 'L' , 50 },{ 'C' , 100 },{ 'D' , 500 },{ 'M' , 1000 },
+            };
+
+            int number = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i != s.Length - 1 && romanDic[s[i]] < romanDic[s[i + 1]])
+                {
+                    number += romanDic[s[i + 1]] - romanDic[s[i]];
+                    i++;
+                }
+                else
+                {
+                    number += romanDic[s[i]];
+                }
+            }
+
+            return number;
         }
     }
 }
