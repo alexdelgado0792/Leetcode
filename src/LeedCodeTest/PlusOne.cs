27d86010678b7499c25591b05aec8e86d26359f5
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-21 12:35:06 -0500

    Plus One problem
    solution and test case

---
+++ src/LeedCodeTest/PlusOne.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Problems.PlusOne;
+
+namespace LeedCodeTest
+{
+    public class PlusOne
+    {
+        [Theory]
+        [InlineData(new object[] { new int[] { 3, 9, 9, 9 } })]
+        [InlineData(new object[] { new int[] { 9, 9, 9 } })]
+        [InlineData(new object[] { new int[] { 0 } })]
+        [InlineData(new object[] { new int[] { 2,5 } })]
+        [InlineData(new object[] { new int[] { 4, 3, 2, 1 } })]
+        public void Pass(int[] num)
+        {
+           Solution.PlusOne(num);
+        }
+    }
+}
---
+++ src/Problems/PlusOne/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problems.PlusOne
+{
+    public static class Solution
+    {
+        public static int[] PlusOne(int[] digits)
+        {
+            var result = new Stack<int>();
+            int carry = 1;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                var sum = digits[i] + carry;
+                carry = sum / 10;
+
+                result.Push(sum % 10);
+            }
+
+            if (carry > 0)
+            {
+                result.Push(carry);
+            }
+
+            return result.ToArray();
+        }
+
+    }
+}
