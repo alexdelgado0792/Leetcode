65a3ca5399c6be873d227ca5cfc99933a29f9004
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-20 19:18:34 -0500

    RemoveDuplicates problem
    test and solution

---
+++ src/LeedCodeTest/RemoveDuplicates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Problems.RemoveDuplicatesfromSortedArray;
+
+namespace LeedCodeTest
+{
+    public class RemoveDuplicates
+    {
+        [Theory]
+        [InlineData(new object[] { new int[] { 1, 1, 2 }, 2 })]
+        public void TwoSumPass(int[] input, int result)
+        {
+            Assert.Equal(result, Solution.RemoveDuplicates(input));
+        }
+    }
+}
---
+++ src/Problems/RemoveDuplicatesfromSortedArray/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.RemoveDuplicatesfromSortedArray
+{
+    public static class Solution
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+
+            }
+            return i + 1;
+        }
+
+        public static int[] RemoveDuplicate2(int[] nums)
+        {
+            var new_nums = new List<int>();
+
+            foreach (var number in nums)
+            {
+                if (!new_nums.Contains(number))
+                {
+                    new_nums.Add(number);
+                }
+            }
+
+            return new_nums.ToArray();
+        }
+    }
+}
