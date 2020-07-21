c750902edf7798d38a5bd8e5a86cec98b0ea0719
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-20 19:59:47 -0500

    Lenght Last Word
    problem solution and test

---
+++ src/LeedCodeTest/LengthOfLastWord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Problems.LenghtLastWord;
+
+namespace LeedCodeTest
+{
+    public class LengthOfLastWord
+    {
+        [Theory]
+        [InlineData("HelloWorld")]
+        [InlineData("Hello World")]
+        [InlineData("a ")]
+        [InlineData("")]
+        [InlineData("One of the things ")]
+        public void Pass(string s)
+        {
+            Assert.Equal(5, Solution.LengthOfLastWord(s));
+        }
+    }
+}
---
+++ src/Problems/LenghtLastWord/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.LenghtLastWord
+{
+    public static class Solution
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            return s.Trim().Split(' ')[^1].Length;
+        }
+    }
+}
