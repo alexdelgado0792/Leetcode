9ea969b511b366e5ebca32e45ce48d4ff64afcd6
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-07-20 19:30:27 -0500

    Implement StrStr
    problem and test template

---
+++ src/LeedCodeTest/StrStr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Problems.Implement_strStr;
+
+namespace LeedCodeTest
+{
+    public class StrStr
+    {
+        [Theory]
+        [InlineData("hello", "ll")]
+        [InlineData("aaaaa", "bba")]
+        [InlineData("aaaaa", "aaaaa")]
+        public void Pass(string haystack, string needle)
+        {
+            //Solution.StrStr(haystack,needle);
+        }
+    }
+}
---
+++ src/Problems/Implement strStr/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.Implement_strStr
+{
+    public static class Solution
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+            if (needle == string.Empty)
+            {
+                return 0;
+            }
+
+            return haystack.IndexOf(needle);
+        }
+    }
+}
